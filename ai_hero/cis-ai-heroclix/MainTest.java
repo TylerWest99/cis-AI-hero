7ea0713c7ca21d058f19638de8418df9a2930bd0
Tyler West <tyler@TylerPC.localdomain>
2020-11-21 15:24:21 -0500

    adds java

--- ai_hero/cis-ai-heroclix/MainTest.java
+++ ai_hero/cis-ai-heroclix/MainTest.java
@@ -125,101 +125,99 @@ public class MainTest {
 	}
 		
 		
-
-		
 		//new basic alg.
-		static boolean is(int startRow, int startCol, Board t, Stack s, int goalRow, int goalCol) {
-			int tmpId = 0;
-			int tmprow = 0;
-			int tmpcol = 0;
-			boolean goalTopRight = false;
-			boolean goalTopLeft = false;
-			boolean goalBottomLeft = false;
-			boolean goalBottomRight = false;
-			
-			boolean startTopLeft = false;
-			boolean startTopRight = false;
-			boolean startBottomLeft = false;
-			boolean startBottomRight = false;
-			
-			if(startRow < 8 && startCol < 8) {
-				startTopLeft = true;
-			}
-			if(startRow < 8 && startCol >= 8) {
-				startTopRight = true;
-			}
-			if(startRow >= 8 && startCol >= 8) {
-				startBottomRight = true;
-			}
-			if(startRow < 8 && startCol >= 8) {
-				startBottomLeft = true;
-			}
-			if(goalRow >= 8 && goalCol >= 8) {
-				goalBottomRight = true;
-			}
-			if(goalRow < 8 && goalCol >= 8) {
-				goalTopRight = true;
-			}
-			if(goalRow < 8 && goalCol < 8 ) {
-				goalTopLeft = true;
-			}
-			if(goalRow >= 8 && goalCol < 8) {
-				goalBottomLeft = true;
-			}
-			char c;
-			boolean found = false;
-			
-			t.boards[startRow][startCol].setHasVisted(true);
-			s.push(t.boards[startRow][startCol]);
-			c = (char)(startCol + 'a');
-			System.out.print(c + "" + (startRow+1) + "->");
-			
-			if(startCol == goalCol && startRow == goalRow) {
-				System.out.println("Goal Found!");
-				found = true;
-				return found;
-			}else {
-				//the next 4 ifs are for going to bootom right goal the other 4 ends need to be specified later
-				if(startTopLeft && goalBottomRight) {
-					goSE(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-					goE(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-					goS(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-					goNE(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-					goSW(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-					goN(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-					goW(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+				static boolean is(int startRow, int startCol, Board t, Stack s, int goalRow, int goalCol) {
+					int tmpId = 0;
+					int tmprow = 0;
+					int tmpcol = 0;
+					boolean goalTopRight = false;
+					boolean goalTopLeft = false;
+					boolean goalBottomLeft = false;
+					boolean goalBottomRight = false;
+					
+					boolean startTopLeft = false;
+					boolean startTopRight = false;
+					boolean startBottomLeft = false;
+					boolean startBottomRight = false;
+					
+					if(startRow < 8 && startCol < 8) {
+						startTopLeft = true;
+					}
+					if(startRow < 8 && startCol >= 8) {
+						startTopRight = true;
+					}
+					if(startRow >= 8 && startCol >= 8) {
+						startBottomRight = true;
+					}
+					if(startRow < 8 && startCol >= 8) {
+						startBottomLeft = true;
+					}
+					if(goalRow >= 8 && goalCol >= 8) {
+						goalBottomRight = true;
+					}
+					if(goalRow < 8 && goalCol >= 8) {
+						goalTopRight = true;
+					}
+					if(goalRow < 8 && goalCol < 8 ) {
+						goalTopLeft = true;
+					}
+					if(goalRow >= 8 && goalCol < 8) {
+						goalBottomLeft = true;
+					}
+					char c;
+					boolean found = false;
+					
+					t.boards[startRow][startCol].setHasVisted(true);
+					s.push(t.boards[startRow][startCol]);
+					c = (char)(startCol + 'a');
+					System.out.print(c + "" + (startRow+1) + "->");
+					
+					if(startCol == goalCol && startRow == goalRow) {
+						System.out.println("Goal Found!");
+						found = true;
+						return found;
+					}else {
+						//the next 4 ifs are for going to bootom right goal the other 4 ends need to be specified later
+						if(startTopLeft && goalBottomRight) {
+							goSE(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+							goE(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+							goS(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+							goNE(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+							goSW(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+							goN(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+							goW(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+						}
+						if(startTopRight && goalBottomRight) {
+							goS(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+							goSW(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+							goSE(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+							goW(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+							goE(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+							goNW(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+							goNE(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+						}
+						if(startBottomLeft && goalBottomRight) {
+							goW(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+							goNW(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+							goSW(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+							goN(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+							goS(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+							goNE(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+							goSE(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+						}
+						if(startBottomRight && goalBottomRight) {
+							//do some code still need ide but testing with this
+							goSE(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+							goE(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+							goS(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+							goNE(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+							goSW(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+							goN(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+							goW(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
+						}
+					}
+					return true;
 				}
-				if(startTopRight && goalBottomRight) {
-					goS(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-					goSW(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-					goSE(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-					goW(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-					goE(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-					goNW(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-					goNE(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-				}
-				if(startBottomLeft && goalBottomRight) {
-					goW(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-					goNW(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-					goSW(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-					goN(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-					goS(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-					goNE(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-					goSE(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-				}
-				if(startBottomRight && goalBottomRight) {
-					//do some code still need ide but testing with this
-					goSE(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-					goE(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-					goS(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-					goNE(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-					goSW(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-					goN(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-					goW(tmpId, tmprow, tmpcol, startRow, startCol, found, goalRow, goalCol, t, s);
-				}
-			}
-			return true;
-		}
 
 		
 		//Runs the search.
