a14199d30516a64d976c542e511af9009c09f446
Tyler West <tyler@TylerPC.localdomain>
2020-11-24 16:06:47 -0500

    updates locating system

--- ai/src/ai/MainTest.java
+++ ai/src/ai/MainTest.java
@@ -2,129 +2,137 @@ package ai;
 import java.util.*;
 
 public class MainTest {
-	
-	//Used for testing. Prints out the node and all connected nodes. (-1 for walls)
-	static void printBlock(int i, int j,Board t) {
-		System.out.println("_______________________");
-		
-		System.out.println(i + "," + j);
-		System.out.println(t.boards[i][j].nw + " " + t.boards[i][j].n + " " + t.boards[i][j].ne);
-		System.out.println(t.boards[i][j].w + " " + t.boards[i][j].getId() + " " + t.boards[i][j].e);
-		System.out.println(t.boards[i][j].sw + " " + t.boards[i][j].s + " " + t.boards[i][j].se);
-	}
-	
-	//Goes east
-	static void goE(int startRow, int startCol, Board t){
-		int tmpId = t.boards[startRow][startCol].e;
-		int tmpRow = tmpId/16;
-		int tmpCol = tmpId%16;
-		if(tmpId != -1) {
-			System.out.println(Integer.toString(tmpRow)+Integer.toString(tmpCol));
+	//takes in a id position and "YX" y is up down x is left right
+	//method gets and returns new name id with A1, B2, A4, etc format
+		public static String getName(String id) {
+			String name;
+			char row = id.charAt(0);
+			char col = id.charAt(1);
+			String rows = Character.toString(row);
+			String cols = Character.toString(col);
+			String col2 = "";
+			String row2 = "";
+			String[] lettersArray = new String[]{"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P"};
+			String[] numsArray = new String[] {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"};
+				
+			for(int i = 0; i < lettersArray.length; i++) {
+				String index = String.valueOf(i);
+				if(index.equals(cols)) {
+					col2 = lettersArray[i];
+				}
+			}
+			for(int j = 0; j < numsArray.length; j++) {
+				String index2 = String.valueOf(j);
+				if(index2.equals(rows)) {
+					row2 = numsArray[j];
+				}
+			}
+			name = col2 + row2;
+			return name;
 		}
-	}
-	
-	//Goes west
-	static void goW(int startRow, int startCol, Board t){
-		int tmpId = t.boards[startRow][startCol].w;
-		int tmpRow = tmpId/16;
-		int tmpCol = tmpId%16;
-		if(tmpId != -1) {
-			System.out.println(Integer.toString(tmpRow)+Integer.toString(tmpCol));
+			
+		//Goes east
+		static String goE(int startRow, int startCol, Board t){
+			int tmpId = t.boards[startRow][startCol].e;
+			int tmpRow = tmpId/16;
+			int tmpCol = tmpId%16;
+			String pos = "None";
+			if(tmpId != -1) {
+				pos = Integer.toString(tmpRow)+Integer.toString(tmpCol);
+			}
+			return getName(pos);
 		}
-	}
-	
-	//Goes north
-	static void goN(int startRow, int startCol, Board t){
-		int tmpId = t.boards[startRow][startCol].n;
-		int tmpRow = tmpId/16;
-		int tmpCol = tmpId%16;
-		if(tmpId != -1) {
-			System.out.println(Integer.toString(tmpRow)+Integer.toString(tmpCol));
+			
+		//Goes west
+		static String goW(int startRow, int startCol, Board t){
+			int tmpId = t.boards[startRow][startCol].w;
+			int tmpRow = tmpId/16;
+			int tmpCol = tmpId%16;
+			String pos = "None";
+			if(tmpId != -1) {
+				pos = Integer.toString(tmpRow)+Integer.toString(tmpCol);
+			}
+			return getName(pos);
 		}
-	}
-	
-	//Goes south
-	static void goS(int startRow, int startCol, Board t){
-		int tmpId = t.boards[startRow][startCol].s;
-		int tmpRow = tmpId/16;
-		int tmpCol = tmpId%16;
-		if(tmpId != -1) {
-			System.out.println(Integer.toString(tmpRow)+Integer.toString(tmpCol));
+			
+		//Goes north
+		static String goN(int startRow, int startCol, Board t){
+			int tmpId = t.boards[startRow][startCol].n;
+			int tmpRow = tmpId/16;
+			int tmpCol = tmpId%16;
+			String pos = "None";
+			if(tmpId != -1) {
+				pos = Integer.toString(tmpRow)+Integer.toString(tmpCol);
+			}
+			return getName(pos);
 		}
-	}
-	
-	//Goes north east
-	static void goNE(int startRow, int startCol, Board t){
-		int tmpId = t.boards[startRow][startCol].ne;
-		int tmpRow = tmpId/16;
-		int tmpCol = tmpId%16;
-		if(tmpId != -1) {
-			System.out.println(Integer.toString(tmpRow)+Integer.toString(tmpCol));
+			
+		//Goes south
+		static String goS(int startRow, int startCol, Board t){
+			int tmpId = t.boards[startRow][startCol].s;
+			int tmpRow = tmpId/16;
+			int tmpCol = tmpId%16;
+			String pos = "None";
+			if(tmpId != -1) {
+				pos = Integer.toString(tmpRow)+Integer.toString(tmpCol);
+			}
+			return getName(pos);
 		}
-	}
-	
-	//Goes south east
-	static void goSE(int startRow, int startCol, Board t){
-		int tmpId = t.boards[startRow][startCol].se;
-		int tmpRow = tmpId/16;
-		int tmpCol = tmpId%16;
-		if(tmpId != -1) {
-			System.out.println(Integer.toString(tmpRow)+Integer.toString(tmpCol));
+			
+		//Goes north east
+		static String goNE(int startRow, int startCol, Board t){
+			int tmpId = t.boards[startRow][startCol].ne;
+			int tmpRow = tmpId/16;
+			int tmpCol = tmpId%16;
+			String pos = "None";
+			if(tmpId != -1) {
+				pos = Integer.toString(tmpRow)+Integer.toString(tmpCol);
+			}
+			return getName(pos);
 		}
-	}
-	
-	//Goes south west
-	static void goSW(int startRow, int startCol, Board t){
-		int tmpId = t.boards[startRow][startCol].sw;
-		int tmpRow = tmpId/16;
-		int tmpCol = tmpId%16;
-		if(tmpId != -1) {
-			System.out.println(Integer.toString(tmpRow)+Integer.toString(tmpCol));
+			
+		//Goes south east
+		static String goSE(int startRow, int startCol, Board t){
+			int tmpId = t.boards[startRow][startCol].se;
+			int tmpRow = tmpId/16;
+			int tmpCol = tmpId%16;
+			String pos = "None";
+			if(tmpId != -1) {
+				pos = Integer.toString(tmpRow)+Integer.toString(tmpCol);
+			}
+			return getName(pos);
 		}
-	}
-	
-	//Goes north west
-	static void goNW(int startRow, int startCol, Board t){
-		int tmpId = t.boards[startRow][startCol].nw;
-		int tmpRow = tmpId/16;
-		int tmpCol = tmpId%16;
-		if(tmpId != -1) {
-			System.out.println(Integer.toString(tmpRow)+Integer.toString(tmpCol));
+			
+		//Goes south west
+		static String goSW(int startRow, int startCol, Board t){
+			int tmpId = t.boards[startRow][startCol].sw;
+			int tmpRow = tmpId/16;
+			int tmpCol = tmpId%16;
+			String pos = "None";
+			if(tmpId != -1) {
+				pos = Integer.toString(tmpRow)+Integer.toString(tmpCol);
+			}
+			return getName(pos);
 		}
-	}
-	
-	//Depth first search algorithm. The program will always travel to the east node, then clockwise if it can't travel east.
-	static boolean dfs(int startRow, int startCol, Board t, Stack s) {
-		int tmpId = 0;
-		int tmprow = 0;
-		int tmpcol = 0;
-		char c;
-		boolean found;
-		
-		t.boards[startRow][startCol].setHasVisted(true);
-		s.push(t.boards[startRow][startCol]);
-		c = (char)(startCol + 'a');
-		System.out.print(c + "" + (startRow+1) + "->");
-		
-		if(t.boards[startRow][startCol].isGoal()) {
-			System.out.println("Goal Found!");
-			return true;
-		}
-			return true;
+			
+		//Goes north west
+		static String goNW(int startRow, int startCol, Board t){
+			int tmpId = t.boards[startRow][startCol].nw;
+			int tmpRow = tmpId/16;
+			int tmpCol = tmpId%16;
+			String pos = "None";
+			if(tmpId != -1) {
+				pos = Integer.toString(tmpRow)+Integer.toString(tmpCol);
+			}
+			return getName(pos);
 		}
 	
-	
-	//Runs the search.
 	public static void main(String[] args) {
 		Stack nodeStack = new Stack();
 		Board test = new Board();
+		//Movement m = new Movement();
 		boolean isFound;
 
 		test.createMap();
-		//test.boards[9][9].setGoal(true); //SET GOAL NODE HERE!
-		//isFound = dfs(2,2,test,nodeStack);
-		
-		goE(0,0,test);
-
 	}
 }
