c8d83f1fb2c8b124643b46c168e960eda4b54cb0
Tyler West <tyler@TylerPC.localdomain>
2020-11-26 23:48:13 -0500

    fully functioning get all moves function

--- ai/src/ai/MainTest.java
+++ ai/src/ai/MainTest.java
@@ -18,10 +18,11 @@ public class MainTest {
 		//gets surrounds
 		List<String> list = Movement.getMoves1(1,12 , t);
 		//uses surrounds to get what is surronding the surrounds
-		//List<String> newList = Movement.getMovesFromList(list, t);
-		for(int i = 0; i< list.size(); i++) {
-			print(list.get(i));
+		List<String> newList = Movement.getAllMoves("G16",1, t);
+		for(int i = 0; i< newList.size(); i++) {
+			print(newList.get(i));
 		}
+		print(newList.size());
 	}
 }
 
